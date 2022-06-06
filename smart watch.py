c0cd7bd115a72dd343615fcbf6e40ec18cc3ebe9
AntrikshSaini13 <106952116+AntrikshSaini13@users.noreply.github.com>
2022-06-06 12:36:51 +0530

    Add files via upload

---
+++ smart watch.py
@@ -0,0 +1,161 @@
+# MODULE OF TKINTER
+
+from tkinter import *
+
+# MODULE OF DATE AND TIME
+
+import datetime
+
+#********************************   USEING THE FUNCTON  *******************************
+
+def date_time():
+
+    time = datetime.datetime.now()
+
+    hr = time.strftime("%I")
+    min = time.strftime("%M")
+    sec = time.strftime("%S")
+    am = time.strftime("%p")
+    date = time.strftime("%d")
+    month = time.strftime("%m")
+    year = time.strftime("%y")
+    day = time.strftime("%a")
+
+    lab_hr.config(text=hr)
+    lab_min.config(text=min)
+    lab_sec.config(text=sec)
+    lab_am_pm.config(text=am)
+    lab_date.config(text=date)
+    lab_mo.config(text=month)
+    lab_year.config(text=year)
+    lab_day.config(text=day)
+
+
+    lab_hr.after(200,date_time)
+
+# interface of clock use the tkinter
+
+clock = Tk()
+
+clock.title("  ****  DIGITAL CLOCK  ****  ")
+clock.geometry("1500x500")
+clock.config(bg="black")
+
+#                             use the Label function
+
+#**********************************  HOURS  ************************************
+
+lab_hr = Label(clock,text="00",font=("Time New Roman",60,"bold"),
+             bg="red",fg="white")
+lab_hr.place(x=120,y=50,height=110,width=100)
+
+lab_hr_txt = Label(clock,text="hour",font=("Time New Roman",20,"bold"),
+             bg="red",fg="white")
+lab_hr_txt.place(x=120,y=190,height=30,width=100)
+
+#*********************************   MINUTES  ***********************************
+
+lab_min = Label(clock,text="00",font=("Time new Roman",60,"bold"),
+             bg="red",fg="white")
+lab_min.place(x=340,y=50,height=110,width=100)
+
+lab_min_txt = Label(clock,text="min",font=("Time New Roman",20,"bold"),
+             bg="red",fg="white")
+lab_min_txt.place(x=340,y=190,height=30,width=100)
+
+
+#**********************************  SECOND   *************************************
+
+lab_sec = Label(clock,text="00",font=("Time new Roman",60,"bold"),
+             bg="red",fg="white")
+lab_sec.place(x=560,y=50,height=110,width=100)
+
+lab_sec_txt = Label(clock,text="sec",font=("Time New Roman",20,"bold"),
+             bg="red",fg="white")
+lab_sec_txt.place(x=560,y=190,height=30,width=100)
+
+#*********************************  AM & PM  ************************************
+
+lab_am_pm = Label(clock,text="00",font=("Time new Roman",50,"bold"),
+             bg="red",fg="white")
+lab_am_pm.place(x=780,y=50,height=110,width=100)
+
+lab_am_pm_txt = Label(clock,text="AM/PM",font=("Time New Roman",20,"bold"),
+             bg="red",fg="white")
+lab_am_pm_txt.place(x=780,y=190,height=30,width=100)
+
+#**********************************   DATE   *************************************
+
+lab_date = Label(clock,text="00",font=("Time New Roman",60,"bold"),
+             bg="red",fg="white")
+lab_date.place(x=120,y=270,height=110,width=100)
+
+lab_date_txt = Label(clock,text="date",font=("Time New Roman",20,"bold"),
+            bg="red",fg="white")
+lab_date_txt.place(x=120,y=410,height=30,width=100)
+
+#***********************************  MONTH  *************************************
+
+lab_mo = Label(clock,text="00",font=("Time new Roman",60,"bold"),
+             bg="red",fg="white")
+lab_mo.place(x=340,y=270,height=110,width=100)
+
+lab_mo_txt = Label(clock,text="month",font=("Time New Roman",20,"bold"),
+             bg="red",fg="white")
+lab_mo_txt.place(x=340,y=410,height=30,width=100)
+
+#************************************  YEAR  **************************************
+
+lab_year = Label(clock,text="00",font=("Time new Roman",60,"bold"),
+             bg="red",fg="white")
+lab_year.place(x=560,y=270,height=110,width=100)
+
+lab_year_txt = Label(clock,text="Year",font=("Time New Roman",20,"bold"),
+             bg="red",fg="white")
+lab_year_txt.place(x=560,y=410,height=30,width=100)
+
+#*************************************  DAY  ****************************************
+
+lab_day = Label(clock,text="00",font=("Time new Roman",35,"bold"),
+             bg="red",fg="white")
+lab_day.place(x=780,y=270,height=110,width=100)
+
+lab_day_txt = Label(clock,text="Day",font=("Time New Roman",20,"bold"),
+             bg="red",fg="white")
+lab_day_txt.place(x=780,y=410,height=30,width=100)
+"""""""""""""""""
+#************************************* WEATHER **************************************
+
+#**********************************  TEMPERATURE  ***********************************
+
+lab_temp = Label(clock,text="00",font=("Time New Roman",60,"bold"),
+             bg="red",fg="white")
+lab_temp.place(x=120,y=270,height=110,width=100)
+
+lab_temp_txt = Label(clock,text="date",font=("Time New Roman",20,"bold"),
+            bg="red",fg="white")
+lab_temp_txt.place(x=120,y=410,height=30,width=100)
+
+#*************************************  CELCEIUS  ************************************
+
+lab_mo = Label(clock,text="00",font=("Time new Roman",60,"bold"),
+             bg="red",fg="white")
+lab_mo.place(x=340,y=270,height=110,width=100)
+
+lab_mo_txt = Label(clock,text="month",font=("Time New Roman",20,"bold"),
+             bg="red",fg="white")
+lab_mo_txt.place(x=340,y=410,height=30,width=100)
+"""""""""
+
+
+
+
+date_time()
+clock.mainloop()
+
+
+
+
+
+
+
